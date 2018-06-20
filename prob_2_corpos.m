ca6068c8e143e3dadab6cd40697a6dbc31e5d931
allanternaverde <allanternaverde@gmail.com>
2018-06-20 16:06:33 -0300

    Update prob_2_corpos.m

--- prob_2_corpos.m
+++ prob_2_corpos.m
@@ -1,15 +1,21 @@
+% limpa framework
 clear all;
 close all;
 clc;
+
 % vetores iniciais
+% posicao inicial
 r_0 = [205.081; 5393.556; -5866.674]; % [km]
+%r_0 = [10016.34; -17012.52; 7899.28];
+% velocidade inicial
 v_0 = [-5.518; 6.72; 2.901]; % [km/s]
+%v_0 = [2.5; -1.05; 3.88];
+
 % vetor de condições iniciais
 X_0 = [r_0; v_0];
 
 % parametro gravitacional par uma orbita Terrestr u = G * m_Terra
 u = 3.986e5;
-
 % energia total específica orbital
 E = norm(v_0)^2/2 - u/norm(r_0);
 % semieixo maior da órbita
@@ -17,103 +23,89 @@ a = -u/(2*E);
 % período da órbita
 T = 2*pi*sqrt(a^3/u);
 
-% Sistema de equações para o sistema dos dois corpos
+% Sistema de equações para o problema dos dois corpos
 doisCorpos = @(t, X) [zeros(3,3), eye(3); -(u/norm(X(1:3,1))^3)*eye(3), zeros(3,3)]*X;
 
-% o tempo de integração, será feito igual a um período orbinal
+% ajuste da tolerância relativa do integrador
 options = odeset('RelTol',1e-9);
-[t X] = ode45(doisCorpos, [0 10*T], X_0, options);
 
+% numero de órbitas a serem propagadas 
+nOrbitas = 1;
 
-% posição e velocidade de primeira orbita propagada
-r_i = [ X(1,1) X(1,2) X(1,3)];
-v_i = [ X(1,4) X(1,5) X(1,6)];
-% posição e velocidade de primeira orbita propagada
-r_f = [ X(end,1) X(end,2) X(end,3)];
-v_f = [ X(end,4) X(end,5) X(end,6)];
-% energia total específica orbital para primeira orbita
-E_i = norm(v_i)^2/2 - u/norm(r_i);
-% energia total específica orbital para ultima orbita
-E_f = norm(v_f)^2/2 - u/norm(r_f);
-% semieixo maior da primeira órbita
-a_i = -u/(2*E_i);
-% semieixo maior da ultima órbita
-a_f = -u/(2*E_f);
-% plot dos vetores de posição
+% integração numérica das equações doisCorpos considerando as condicoes
+% iniciais X_0, para integração é utilizado um tempo múltiplo do período
+% orbital
+[t X] = ode45(doisCorpos, [0 nOrbitas*T], X_0, options);
 
-disp(['discrepancia = ',num2str(a_i-a_f),' [km]']);
+%% figura 1 - plot 3D animado da órbita
+fig = figure;
 plot3(X(:,1),X(:,2),X(:,3),'LineWidth',2,'Color','red');
-hold;
+hold on;
+p = plot3(X(1,1),X(1,2),X(1,3),'o','MarkerFaceColor','blue');
+vel = quiver3(X(1,1),X(1,2),X(1,3),X(1,4),X(1,5),X(1,6),'AutoScaleFactor',2000,'LineWidth',2,'Color','blue','MaxHeadSize',2);
 
-%view(cross(r_0,v_0));
+% plot do elipsóide terrestre utilizando o modelo GRS80
+earth = referenceEllipsoid('GRS80','km');
+[x, y, z] = ellipsoid(0,0,0,earth.SemimajorAxis, earth.SemimajorAxis, earth.SemiminorAxis);
+globe = surf(x, y, -z, 'FaceColor', 'none', 'EdgeColor', 0.5*[1 1 1]);
+image_file = 'https://noperation.files.wordpress.com/2012/11/world32k.jpg';
+cdata = imread(image_file);
+set(globe, 'FaceColor', 'texturemap', 'CData', cdata,'EdgeColor', 'none');
 
-% % o tempo de integração, será feito igual a um período orbinal
-% [t X] = ode15s(doisCorpos, [0 T], X_0);
-% 
-% % plot dos vetores de posição
-% plot3(X(:,1),X(:,2),X(:,3),'b');
-% 
-% % o tempo de integração, será feito igual a um período orbinal
-% [t X] = ode23s(doisCorpos, [0 T], X_0);
-% 
-% % plot dos vetores de posição
-% plot3(X(:,1),X(:,2),X(:,3),'r');
-
-% % plot do modelo terrestre
-% r_e = 6378.137; % raio equatorial
-% r_p = 6356.752; % raio polar
-% [x, y, z] = referenceEllipsoid(0,0,0,r_e,r_e,r_p,30);
-% surf(x, y, z);
-%grs80 = referenceEllipsoid('GRS80','km');
-
-
-% % PLOTAGEM DA TERRA NO MODELO DE ELIPSOIDE 'GRS80'
-% grs80 = referenceEllipsoid('GRS80','km');
-% figure('Renderer','opengl');
-% ax = axesm('globe','Geoid',grs80,'Grid','on', ...
-%     'GLineWidth',1,'GLineStyle','-',...
-%     'Gcolor',[0.9 0.9 0.1],'Galtitude',100);
-% ax.Position = [0 0 1 1];
-% view(3)
-% load topo
-% geoshow(topo,topolegend,'DisplayType','texturemap')
-% demcmap(topo)
-% land = shaperead('landareas','UseGeoCoords',true);
-% plotm([land.Lat],[land.Lon],'Color','black')
-% rivers = shaperead('worldrivers','UseGeoCoords',true);
-% plotm([rivers.Lat],[rivers.Lon],'Color','blue')
-
-
+% unidades de opções do gráfico
 xlabel('[km]');
 ylabel('[km]');
 zlabel('[km]');
 axis equal;
+ax = gca;
+xlim(ax.XLim*1.1);
+ylim(ax.YLim*1.1);
+zlim([ax.ZLim(1)*2 ax.ZLim(2)]);
+axis manual;
 grid;
 
+n = 13;
+for k = 1:n:length(X(:,1))
+    set(p, 'XData', X(k,1));
+    set(p, 'YData', X(k,2));
+    set(p, 'ZData', X(k,3));
+    set(vel, 'XData', X(k,1));
+    set(vel, 'yData', X(k,2));
+    set(vel, 'ZData', X(k,3));
+    set(vel, 'UData', X(k,4));
+    set(vel, 'VData', X(k,5));
+    set(vel, 'WData', X(k,6));
+     if k > 1
+         delay = (t(k)-t(k-n))/10000;
+         pause(delay);
+     end
+    drawnow;
+end
+hold off;
 
-% lat = atan(X(:,3)./sqrt(X(:,1).^2 + X(:,2).^2))*180/pi;
-% for i=1:length(X(:,1))
-%     lon(i,1) = acos(X(i,1)/sqrt(X(i,1)^2 + X(i,2)^2));
-%     if X(i,2) < 0
-%         lon(i,1) = -lon(i,1);
-%     end
-%     lon(i,1) = lon(i,1)*180/pi;
-% end
-%plot(x,y);
-%grid minor;
-%plot(lon,lat,'.');
+%% figura 2 - plano orbital e vetores de velocidade
+fig2 = figure;
+plot3(X(:,1),X(:,2),X(:,3),'LineWidth',2,'Color','red');
+hold on;
 
-grs80 = referenceEllipsoid('GRS80','km');
-[lat,lon,h] = ecef2geodetic(grs80,X(:,1),X(:,2),X(:,3));
-load geoid
-figure
-axesm eckert4; 
-framem; gridm;
-axis off;
-geoshow(geoid, geoidrefvec, 'DisplayType', 'texturemap');
-hcb = colorbar('southoutside');
-set(get(hcb,'Xlabel'),'String','EGM96 Geoid Heights in Meters.')
-geoshow('landareas.shp', 'FaceColor', [0.5 1.0 0.5]);
-geoshow(lat,lon,'LineWidth',2 ,'Color','red');
+% plot do elipsóide terrestre utilizando o modelo GRS80
+earth = referenceEllipsoid('GRS80','km');
+[x, y, z] = ellipsoid(0,0,0,earth.SemimajorAxis, earth.SemimajorAxis, earth.SemiminorAxis);
+globe = surf(x, y, -z, 'FaceColor', 'none', 'EdgeColor', 0.5*[1 1 1]);
+image_file = 'https://noperation.files.wordpress.com/2012/11/world32k.jpg';
+cdata = imread(image_file);
+set(globe, 'FaceColor', 'texturemap', 'CData', cdata,'EdgeColor', 'none');
 
+% plot vetores de velocidade no plano orbital
+fim = int32(length(X(:,1))/nOrbitas);
+for i=1:40:fim
+    quiver3(X(i,1),X(i,2),X(i,3),X(i,4),X(i,5),X(i,6),'AutoScaleFactor',1200,'LineWidth',2,'Color','blue','MaxHeadSize',3);
+end
 
+% unidades de opções do gráfico
+xlabel('[km]');
+ylabel('[km]');
+zlabel('[km]');
+view(cross(r_0,v_0));
+axis equal;
+grid;
