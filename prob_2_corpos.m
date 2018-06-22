ae2c29c0fd4deb3f0e2be3a268c6193441927014
allanternaverde <allanternaverde@gmail.com>
2018-06-22 17:31:14 -0300

    Update prob_2_corpos.m

--- prob_2_corpos.m
+++ prob_2_corpos.m
@@ -3,48 +3,48 @@ clear all;
 close all;
 clc;
 
-%% vetores iniciais
-% posicao inicial
+%% vetores de velocidade e posição iniciais
 r_0 = [205.081; 5393.556; -5866.674]; % [km]
-%r_0 = [10016.34; -17012.52; 7899.28];
-% velocidade inicial
 v_0 = [-5.518; 6.72; 2.901]; % [km/s]
-%v_0 = [2.5; -1.05; 3.88];
-%% vetor de condições iniciais
-X_0 = [r_0; v_0];
 
 % parametro gravitacional par uma orbita Terrestr u = G * m_Terra
 u = 3.986e5; %[km^3/s^2]
 % energia total específica orbital
-E = norm(v_0)^2/2 - u/norm(r_0);
+E = norm(v_0)^2/2 - u/norm(r_0); %[kJ/kg]
 % semieixo maior da órbita
-a = -u/(2*E);
+a = -u/(2*E); % [km]
 % período da órbita
-T = 2*pi*sqrt(a^3/u);
+T = 2*pi*sqrt(a^3/u); %[s]
+
+%% vetor de condições iniciais para integração numérica
+X_0 = [r_0; v_0];
 
 %% Sistema de equações para o problema dos dois corpos
 doisCorpos = @(t, X) [zeros(3,3), eye(3); -(u/norm(X(1:3,1))^3)*eye(3), zeros(3,3)]*X;
 
 %% numero de órbitas a serem propagadas 
-nOrbitas = 100;
+nOrbitas = 3;
 
-%% integração numérica das equações doisCorpos considerando as condicoes
-% iniciais X_0, para integração é utilizado um tempo múltiplo do período
-% orbital
-options = odeset('RelTol',1e-9); % ajuste da tolerância relativa do integrador
+%% integração numérica utilizando ODE45
+% ajuste da tolerância relativa 
+options = odeset('RelTol',1e-9); 
+% integração 
 [t X] = ode45(doisCorpos, [0 nOrbitas*T], X_0, options);
 
 %% figura 1 - plot 3D animado da órbita
-% nAnicmoes escolhe o numero de animacoes (deve ser inferior ao numero de orbitas nOrbitas)
+% nAnimacoes escolhe o numero de animacoes 
 nAnimacoes = 1;
 if nAnimacoes > nOrbitas
     nAnimacoes = nOrbitas;
 end
 
 fig = figure;
+% plot das orbitas
 plot3(X(:,1),X(:,2),X(:,3),'LineWidth',2,'Color','red');
 hold on;
+% posicao inicial
 p = plot3(X(1,1),X(1,2),X(1,3),'o','MarkerFaceColor','blue');
+% vetor de posicao inicial
 vel = quiver3(X(1,1),X(1,2),X(1,3),X(1,4),X(1,5),X(1,6),'AutoScaleFactor',2000,'LineWidth',2,'Color','blue','MaxHeadSize',2);
 
 % plot do elipsóide terrestre utilizando o modelo GRS80
@@ -55,7 +55,7 @@ image_file = 'https://noperation.files.wordpress.com/2012/11/world32k.jpg';
 cdata = imread(image_file);
 set(globe, 'FaceColor', 'texturemap', 'CData', cdata,'EdgeColor', 'none');
 
-% unidades de opções do gráfico
+% unidades dos eixos e opções gráficas
 xlabel('[km]');
 ylabel('[km]');
 zlabel('[km]');
@@ -63,6 +63,7 @@ axis equal;
 axis manual;
 grid;
 
+% animação da posição e vetor velocidade
 n = 13;
 for k = 1:n:int32(((length(X(:,1)))/nOrbitas)*nAnimacoes)
     set(p, 'XData', X(k,1));
@@ -84,6 +85,7 @@ hold off;
 
 %% figura 2 - plano orbital e vetores de velocidade
 fig2 = figure;
+% plot das orbitas
 plot3(X(:,1),X(:,2),X(:,3),'LineWidth',2,'Color','red');
 hold on;
 
@@ -101,7 +103,7 @@ for i=1:40:fim
     quiver3(X(i,1),X(i,2),X(i,3),X(i,4),X(i,5),X(i,6),'AutoScaleFactor',1200,'LineWidth',2,'Color','blue','MaxHeadSize',3);
 end
 
-% unidades de opções do gráfico
+% unidades dos eixos e opções do gráfico
 xlabel('[km]');
 ylabel('[km]');
 zlabel('[km]');
@@ -109,9 +111,7 @@ view(cross(r_0,v_0));
 axis equal;
 grid;
 
-%% plot no tempo dos vetores de posição, velocidade e momento angular
-% especifico em módulo
-
+%% plot no tempo dos vetores de posição, velocidade e momento angular especifico em módulo
 for i=1:length(X(:,1))
     R_ = [X(i,1);X(i,2);X(i,3)];
     V_ = [X(i,4);X(i,5);X(i,6)];
@@ -141,12 +141,12 @@ title(ax3, 'Energia total específica');
 ylabel(ax3, 'E [kJ/kg]');
 grid minor;
 
-%% altitudes no apogeu e perigeu
+%% altitudes e velocidades no apogeu e perigeu
 disp('altitude no perigeu');
 disp([num2str(min(R)), ' km']);
 
 disp('altitude no apogeu');
-disp([num2str(max(R)),' km']);
+disp([num2str(max(R)- 6357),' km']);
 
 disp('velocidade no perigeu');
 disp([num2str(max(V)), ' km/s']);
